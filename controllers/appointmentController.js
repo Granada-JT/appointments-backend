5db02d361a4e76deeeb0ac2be90d53070e516e72
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-11-29 02:17:38 +0800

    feat: add backend routes

---
+++ controllers/appointmentController.js
@@ -0,0 +1,16 @@
+
+exports.getAppointments = async(req, res) => {
+
+}
+
+exports.createAppointment = async(req, res) => {
+
+}
+
+exports.editAppointment = async(req, res) => {
+
+}
+
+exports.deleteAppointment = async(req, res) => {
+
+}
