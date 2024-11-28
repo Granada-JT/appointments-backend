76bb917116200ba1ff12e83fc3c08fb3fbf41611
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-11-28 20:36:32 +0800

    feat: add appointment model

---
+++ models/appointment.js
@@ -0,0 +1,15 @@
+const APPOINTMENT_MODEL = `
+	INSERT INTO appointments (
+		patient_name,
+		appointment_start_date,
+		appointment_end_date,
+		appointment_start_time,
+		appointment_end_time,
+		comments
+	)
+	VALUES (?, ?, ?, ?, ?, ?)
+`
+
+module.exports = {
+	APPOINTMENT_MODEL
+};
