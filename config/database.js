fcf63ca9393d8dc0f3f7819016a13b4db65acebe
Jomar Tabios Granada <jomart.granada@gmail.com>
2024-11-29 03:50:36 +0800

    fix: update init database function

--- config/database.js
+++ config/database.js
@@ -1,15 +1,37 @@
 const sqlite3 = require("sqlite3");
 
+let db;
+
 const initDatabase = () => {
-	const db = new sqlite3.Database(':memory:', (error) => {
-		if (error) {
-			console.error('Error Connecting to SQLite database:', error);
-		} else {
-			console.log('Connected to SQLite in-memory database.');
-		}
-	})
+	if (!db) {
+		db = new sqlite3.Database(':memory:', (error) => {
+			if (error) {
+				console.error('Error Connecting to SQLite database:', error);
+			} else {
+				console.log('Connected to SQLite in-memory database.');
+			}
+		})
+	
+		db.run(`
+			CREATE TABLE appointments (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			patient_name TEXT NOT NULL,
+			appointment_start_date TEXT NOT NULL,
+			appointment_end_date TEXT NOT NULL,
+			appointment_start_time TEXT NOT NULL,
+			appointment_end_time TEXT NOT NULL,
+			comments TEXT
+		)
+		`, (error) => {
+			if (error) {
+				console.error("Error creating table:", error);
+			} else {
+				console.log("Appointments table created successfully.");
+			}
+		});
+	}
 
 	return db;
 }
 
-module.exports = initDatabase;
+module.exports = { initDatabase };
